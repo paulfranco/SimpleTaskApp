c4f35db9f94069f13b2b53dd2f1c72a3ed8286b9
Paul Franco <paulfranco@me.com>
2018-12-10 18:06:51 -0800

    Added toobar

--- app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
@@ -2,12 +2,21 @@ package co.paulfran.paulfranco.simpletaskapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
+
+    // Initialize the toolbar
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Bind the toolbar
+        toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Task App");
     }
 }
