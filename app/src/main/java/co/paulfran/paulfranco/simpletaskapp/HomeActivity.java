8285922bbc785aa2b06188964d91eb67e12a2d22
Paul Franco <paulfranco@me.com>
2018-12-10 19:49:57 -0800

    Added FloatingActionButton

--- app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
@@ -1,13 +1,17 @@
 package co.paulfran.paulfranco.simpletaskapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
     // Initialize the toolbar
     private Toolbar toolbar;
+
+    private FloatingActionButton fabBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,15 @@ public class HomeActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_home);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Task App");
+
+        // Bind FAB
+        fabBtn = findViewById(R.id.fab_btn);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
