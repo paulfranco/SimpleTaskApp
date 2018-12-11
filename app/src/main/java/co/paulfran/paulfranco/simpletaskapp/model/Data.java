5b8e73adf945709bdd6a0ab88f25e91242d3c5f3
Paul Franco <paulfranco@me.com>
2018-12-10 19:55:25 -0800

    Added Model class

---
+++ app/src/main/java/co/paulfran/paulfranco/simpletaskapp/model/Data.java
@@ -0,0 +1,55 @@
+package co.paulfran.paulfranco.simpletaskapp.model;
+
+public class Data {
+
+    // Initialize variables
+    private String title;
+    private String note;
+    private String date;
+    private String id;
+
+    // Constructor
+    public Data() {
+
+    }
+    // Constructor
+    public Data(String title, String note, String date, String id) {
+        this.title = title;
+        this.note = note;
+        this.date = date;
+        this.id = id;
+    }
+
+    // Getter and Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
