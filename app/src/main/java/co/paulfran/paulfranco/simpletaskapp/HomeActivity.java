74d9ab62111f8ef026ede7e574e9feb95e06c83f
Paul Franco <paulfranco@me.com>
2018-12-14 02:57:20 -0800

    complete code to insert data into Firebase

--- app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
@@ -5,8 +5,22 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import co.paulfran.paulfranco.simpletaskapp.model.Data;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,6 +28,10 @@ public class HomeActivity extends AppCompatActivity {
     private Toolbar toolbar;
 
     private FloatingActionButton fabBtn;
+
+    // Firebase
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +43,17 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Task App");
 
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser mUser = mAuth.getCurrentUser();
+        String uId = mUser.getUid();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("TaskNote").child(uId);
+
         // Bind FAB
         fabBtn = findViewById(R.id.fab_btn);
 
-        // Click Listerner on Fab that inflates the custom alert dialog
+        // Click Listener on Fab that inflates the custom alert dialog
         fabBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,7 +62,43 @@ public class HomeActivity extends AppCompatActivity {
                 LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
                 View myView = inflater.inflate(R.layout.custominputfield, null);
                 mDialog.setView(myView);
-                AlertDialog dialog = mDialog.create();
+                final AlertDialog dialog = mDialog.create();
+
+                final EditText title = myView.findViewById(R.id.edt_title);
+                final EditText note = myView.findViewById(R.id.edt_note);
+                Button btnSave = myView.findViewById(R.id.btn_save);
+
+                btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        String mTitle = title.getText().toString().trim();
+                        String mNote = note.getText().toString().trim();
+
+                        // Check if fields are empty
+                        if (TextUtils.isEmpty(mTitle)) {
+                            title.setError("Required Field...");
+                            return;
+                        }
+
+                        if (TextUtils.isEmpty(mNote)) {
+                            note.setError("Required Fied...");
+                            return;
+                        }
+
+                        // Generate id and date for the database
+                        String id = mDatabase.push().getKey();
+                        String date = DateFormat.getDateInstance().format(new Date());
+
+                        // Must match the sequence in the model
+                        Data data = new Data(mTitle, mNote, date, id);
+                        mDatabase.child(id).setValue(data);
+
+                        Toast.makeText(getApplicationContext(), "Note Added", Toast.LENGTH_LONG).show();
+
+                        dialog.dismiss();
+                    }
+                });
 
                 dialog.show();
             }
