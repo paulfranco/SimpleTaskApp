4b4162ae8010db9bed10edbd7f0856963ca2eebd
Paul Franco <paulfranco@me.com>
2018-12-14 01:51:04 -0800

    added alert dialog

--- app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
@@ -1,9 +1,11 @@
 package co.paulfran.paulfranco.simpletaskapp;
 
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
@@ -26,10 +28,18 @@ public class HomeActivity extends AppCompatActivity {
         // Bind FAB
         fabBtn = findViewById(R.id.fab_btn);
 
+        // Click Listerner on Fab that inflates the custom alert dialog
         fabBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                AlertDialog.Builder mDialog = new AlertDialog.Builder(HomeActivity.this);
+                LayoutInflater inflater = LayoutInflater.from(HomeActivity.this);
+                View myView = inflater.inflate(R.layout.custominputfield, null);
+                mDialog.setView(myView);
+                AlertDialog dialog = mDialog.create();
+
+                dialog.show();
             }
         });
     }
