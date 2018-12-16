c0b1edd0f606bfe3c64c5a7df2ff8f5804a37272
Paul Franco <paulfranco@me.com>
2018-12-15 20:06:17 -0800

    added logout functionality

--- app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/simpletaskapp/HomeActivity.java
@@ -1,5 +1,6 @@
 package co.paulfran.paulfranco.simpletaskapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
@@ -10,6 +11,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -170,5 +173,23 @@ public class HomeActivity extends AppCompatActivity {
             TextView mDate = myView.findViewById(R.id.date);
             mDate.setText(date);
         }
+    }
+
+    // Menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                mAuth.signOut();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
